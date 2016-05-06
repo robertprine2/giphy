a051040b524734bb96a2b2bcb56d5d1fb9895335
Robert Prine <robertprine2@yahoo.com>
2016-05-06 15:36:59 -0400

    psuedo code complete and started filling out the giff object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,24 @@
-// load js right away, possbily before html is finished loading
+// Waits until html is finished loading then starts JS
 
 $(document).ready(function(){
 
-	//game object to contain variables and methods
+	//giff object to contain variables and methods
 
-	
+	var giff = {
+
+		// Array of marvel characters
+
+		heroes: ["spider-man", ],
+
+	} // Ends giff object
+
+	// populate buttons in array using a for loop, but empty the div first so you can keep adding new buttons
+
+	// on click to add a new button for search and then repopulate the array of buttons
+
+	// on click for giff buttons to populate giffs from api
+
+	// on click for giffs to start and stop
 	
 
-}); // Ends window.onload function
+}); // Ends document.ready function
