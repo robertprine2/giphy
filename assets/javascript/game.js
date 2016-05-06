752bf1b61201bb583dd33368d51fed27f3c37492
Robert Prine <robertprine2@yahoo.com>
2016-05-06 00:07:25 -0400

    initial commit with background image

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// load js right away, possbily before html is finished loading
+
+$(document).ready(function(){
+
+	//game object to contain variables and methods
+
+	
+	
+
+}); // Ends window.onload function
