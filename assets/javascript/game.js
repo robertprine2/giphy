d55cfa47579e3944034e2cbad971f527e849fd38
Robert Prine <robertprine2@yahoo.com>
2016-05-06 16:25:26 -0400

    Attempted to add for loop for buttons, but they aren't showing up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,34 @@ $(document).ready(function(){
 
 		// Array of marvel characters
 
-		heroes: ["spider-man", ],
+		marvelChar: ["spider-man", "wolverine", "psylocke", "beast", "thing", "storm", "professor x", "venom", "magneto", "deadpool", "rogue", "nightcrawler"],
+
+		// Populate buttons
+
+		butPopulate: function() {
+			$("#buttonsView").empty();
+
+			// for loop to make each button in the array
+
+			for (var i = 0; i < giff.marvelChar.length; i++) {
+
+				// Create a button with the name of the character
+
+				var charDiv = $("<button>");
+				charDiv.addClass("char");
+				charDiv.attr("data-name", giff.marvelChar[i]);
+				charDiv.text(giff.marvelChar[i]);
+				$("#buttonsView").append(charDiv);
+				console.log(charDiv);
+
+			}
+		}, // End of butPopulate method
 
 	} // Ends giff object
 
 	// populate buttons in array using a for loop, but empty the div first so you can keep adding new buttons
+
+	giff.butPopulate();
 
 	// on click to add a new button for search and then repopulate the array of buttons
 
