5c30e467352bd1e342b7de0b82043f677d6ebe96
Robert Prine <robertprine2@yahoo.com>
2016-05-07 23:38:10 -0400

    Added button to add hero buttons on the top of the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,9 @@
 
 $(document).ready(function(){
 
-	//giff object to contain variables and methods
+	//gif object to contain variables and methods
 
-	var giff = {
+	var gif = {
 
 		// Array of marvel characters
 
@@ -13,35 +13,66 @@ $(document).ready(function(){
 		// Populate buttons
 
 		butPopulate: function() {
+
 			$("#buttonsView").empty();
 
 			// for loop to make each button in the array
 
-			for (var i = 0; i < giff.marvelChar.length; i++) {
+			for (var i = 0; i < gif.marvelChar.length; i++) {
 
 				// Create a button with the name of the character
 
 				var charDiv = $("<button>");
 				charDiv.addClass("char");
-				charDiv.attr("data-name", giff.marvelChar[i]);
-				charDiv.text(giff.marvelChar[i]);
+				charDiv.attr("data-name", gif.marvelChar[i]);
+				charDiv.text(gif.marvelChar[i]);
 				$("#buttonsView").append(charDiv);
-				console.log(charDiv);
 
-			}
+			} // End of for loop
+
+			console.log(gif.marvelChar);
+
 		}, // End of butPopulate method
 
-	} // Ends giff object
+		// on click to add a new button for search and then repopulate the array of buttons
+		searchClick: function() {
+			$("#addGif").on("click", function(){
+				console.log(gif.marvelChar);
+				// This line of code will grab the input from the textbox
+				var hero = $('#gif-input').val().trim();
+
+				// The hero from the textbox is then added to our array
+				gif.marvelChar.push(hero);
+				
+				// Our array then runs which handles the processing of our marvelChar array
+				gif.butPopulate();
+
+				// *****clear form so it doesn't rise exponentially
+
+				$("#gif-input").val('');
+
+				// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+
+				return false;
+
+			}); // Ends on click addGif function
+		} // Ends searchClick fucntion
+
+	} // Ends gif object
 
 	// populate buttons in array using a for loop, but empty the div first so you can keep adding new buttons
 
-	giff.butPopulate();
+	gif.butPopulate();
 
 	// on click to add a new button for search and then repopulate the array of buttons
 
-	// on click for giff buttons to populate giffs from api
+	// Calls on click for adding a gif
 
-	// on click for giffs to start and stop
+	gif.searchClick();
+
+	// on click for gif buttons to populate gifs from api
+
+	// on click for gifs to start and stop
 	
 
 }); // Ends document.ready function
